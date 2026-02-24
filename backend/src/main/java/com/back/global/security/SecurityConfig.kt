8a67ec3e9cab6d4cc9c63b311e85312006974fe0
Minji-032 <5h6vmm@gmail.com>
2026-02-24 10:01:24 +0900

    refactor: it 사용, return http.build() -> buile()채이닝

--- backend/src/main/java/com/back/global/security/SecurityConfig.kt
+++ backend/src/main/java/com/back/global/security/SecurityConfig.kt
@@ -12,33 +12,27 @@ class SecurityConfig(
     private val customAuthenticationFilter: CustomAuthenticationFilter
 ) {
     @Bean
-    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+    fun filterChain(http: HttpSecurity): SecurityFilterChain =
         http
-            .authorizeHttpRequests{auth ->
-                auth // 허용할 요청 설정
-                    .requestMatchers("/favicon.ico").permitAll()
-                    .requestMatchers("/h2-console/**").permitAll()
+            .authorizeHttpRequests {
+                 // 허용할 요청 설정
+                it.requestMatchers("/favicon.ico").permitAll()
+                it.requestMatchers("/h2-console/**").permitAll()
                     // 회원가입과 로그인은 누구나 들어갈 수 있게 허용
-                    .requestMatchers("/api/v1/user/signup", "/api/v1/user/login").permitAll()
+                it.requestMatchers("/api/v1/user/signup", "/api/v1/user/login").permitAll()
                     // 데이터 요청 가능여부 통과
-                    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                it.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                     // 기타 api 요청은 인가 필요 -> 로그인하지 않으면 제한
-                    .requestMatchers("/api/*/**").authenticated()
+                it.requestMatchers("/api/*/**").authenticated()
                     // 나머지 요청은 허용
-                    .anyRequest().permitAll()
+                it.anyRequest().permitAll()
             }
 
-            .headers { headers ->
-                headers.frameOptions{ frame ->
-                    frame.sameOrigin()
-                }
-            }
-            .csrf { csrf -> csrf.disable() }
+            .headers { it.frameOptions{ frame -> frame.sameOrigin() } }
+            .csrf { it.disable() }
             .addFilterBefore(
                 customAuthenticationFilter,
                 UsernamePasswordAuthenticationFilter::class.java
             )
-
-        return http.build()
-    }
+            .build()
 }
